837c04a1b421f64b34a5224ead487bf65e2a5790
Eduardo Fuentes <egfuentesc@gmail.com>
2024-05-03 20:05:21 -0500

    inicio comit

---
+++ script.js
@@ -0,0 +1,154 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  let success = false; // Variable para indicar si la ejecución fue exitosa
+
+  const browser = await puppeteer.launch({ headless:true, args: ['--no-sandbox'] });
+  //const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  try {
+    await page.setViewport({ width: 1280, height: 800 });
+    await page.goto('https://betplay.com.co/');
+    await page.waitForSelector('input[type="text"][id="userName"]', { visible: true });
+    await page.type('input[type="password"][id="password"]', 'Qwert1619');
+    await page.type('input[type="text"][id="userName"]', '1065623370');
+    await page.click('#btnLoginPrimary');
+    await page.waitForNavigation();
+    await page.goto('https://betplay.com.co/slots/launchGame?gameCode=SPB_aviator&flashClient=true&additionalParam=&integrationChannelCode=PARIPLAY');
+    await new Promise(resolve => setTimeout(resolve, 25000)); // Espera 5000 milisegundos (5 segundos)
+    await page.waitForSelector('#gameFrame');
+    const iframePadre = await page.$('#gameFrame');
+    const framePadre = await iframePadre.contentFrame();
+    await framePadre.waitForSelector('#spribe-game');
+    const iframeInterno = await framePadre.$('#spribe-game');
+    const frameInterno = await iframeInterno.contentFrame();
+
+    //presionar el boton automatico
+    await frameInterno.waitForSelector('body > app-root > app-game > div > div.main-container > div.w-100.h-100 > div > div.game-play > div.bet-controls > app-bet-controls > div > app-bet-control:nth-child(1) > div > app-navigation-switcher > div > button:nth-child(3)');
+    await frameInterno.click('body > app-root > app-game > div > div.main-container > div.w-100.h-100 > div > div.game-play > div.bet-controls > app-bet-controls > div > app-bet-control:nth-child(1) > div > app-navigation-switcher > div > button:nth-child(3)');
+    await new Promise(resolve => setTimeout(resolve, 2000)); // Espera 5000 milisegundos (5 segundos)
+   // await page.screenshot({ path: 'Verificar_1.png' });
+
+   //presione el boton retiro automatico
+    await  frameInterno.waitForSelector('body > app-root > app-game > div > div.main-container > div.w-100.h-100 > div > div.game-play > div.bet-controls > app-bet-controls > div > app-bet-control:nth-child(1) > div > div.second-row > div.cashout-block > div.cash-out-switcher > app-ui-switcher > div');
+    await  frameInterno.click('body > app-root > app-game > div > div.main-container > div.w-100.h-100 > div > div.game-play > div.bet-controls > app-bet-controls > div > app-bet-control:nth-child(1) > div > div.second-row > div.cashout-block > div.cash-out-switcher > app-ui-switcher > div');
+    await new Promise(resolve => setTimeout(resolve, 1000)); // Espera 5000 milisegundos (5 segundos)
+  //  await page.screenshot({ path: 'Verificar_2.png' });
+
+    //Octenemos el saldo del usuario
+    await frameInterno.waitForSelector('body > app-root > app-game > div > div.main-container > div.main-header > app-header > div > div.second-block.d-flex > div.d-flex.h-100 > div.balance.px-2.d-flex.justify-content-end.align-items-center > div > span.amount.font-weight-bold');
+    const elemento = await frameInterno.$('body > app-root > app-game > div > div.main-container > div.main-header > app-header > div > div.second-block.d-flex > div.d-flex.h-100 > div.balance.px-2.d-flex.justify-content-end.align-items-center > div > span.amount.font-weight-bold');
+    const texto = await elemento.evaluate(el => el.textContent);
+    console.log(texto); // Mostrar el texto en la consola
+   // await page.screenshot({ path: 'Verificar_3.png' });
+
+    //ingresamos el valor  de la entrada 
+    await frameInterno.waitForSelector('body > app-root > app-game > div > div.main-container > div.w-100.h-100 > div > div.game-play > div.bet-controls > app-bet-controls > div > app-bet-control:nth-child(1) > div > div.first-row.auto-game-feature > div.bet-block > app-spinner > div > div.input > input');
+    
+    // Evalúa una función en el contexto del navegador para cambiar el valor del input
+    await frameInterno.evaluate(() => {
+         const inputElement = document.querySelector('body > app-root > app-game > div > div.main-container > div.w-100.h-100 > div > div.game-play > div.bet-controls > app-bet-controls > div > app-bet-control:nth-child(1) > div > div.first-row.auto-game-feature > div.bet-block > app-spinner > div > div.input > input');
+         inputElement.value = ' '; // Cambia el valor del input a "100"
+         inputElement.dispatchEvent(new Event('input')); // Dispara un evento 'input' para que se refleje el cambio
+        });
+  const ElementInversion = await frameInterno.$('body > app-root > app-game > div > div.main-container > div.w-100.h-100 > div > div.game-play > div.bet-controls > app-bet-controls > div > app-bet-control:nth-child(1) > div > div.first-row.auto-game-feature > div.bet-block > app-spinner > div > div.input > input');
+   // Simula un clic izquierdo en el botón
+//await ElementInversion.click({ button: 'left' });
+  // Espera un segundo para que se refleje el cambio (opcional)
+  await ElementInversion.type('1000');
+    await new Promise(resolve => setTimeout(resolve, 1000));
+   // await page.screenshot({ path: 'Verificar_4.png' });
+    
+    
+    /*
+    const ElementInversion = await frameInterno.$('body > app-root > app-game > div > div.main-container > div.w-100.h-100 > div > div.game-play > div.bet-controls > app-bet-controls > div > app-bet-control:nth-child(1) > div > div.first-row.auto-game-feature > div.bet-block > app-spinner > div > div.input > input');
+    await ElementInversion.click({ clickCount: 3 }); // Seleccionar todo el texto y borrar
+    await ElementInversion.press('Backspace'); // Oprimir la tecla "Backspace" para borrar
+    await new Promise(resolve => setTimeout(resolve, 1000)); // Espera 5000 milisegundos (5 segundos)
+    await ElementInversion.type('100');
+    await new Promise(resolve => setTimeout(resolve, 1000)); // Espera 5000 milisegundos (5 segundos)
+    await page.screenshot({ path: 'Verificar_4.png' });
+ */
+
+    //agregamos el valor 1.05 de retiro automatico
+
+
+    await frameInterno.waitForSelector('body > app-root > app-game > div > div.main-container > div.w-100.h-100 > div > div.game-play > div.bet-controls > app-bet-controls > div > app-bet-control:nth-child(1) > div > div.second-row > div.cashout-block > div.cashout-spinner-wrapper > div > app-spinner > div > div.input.full-width > input');
+    
+    await frameInterno.evaluate(() => {
+      const inputElementMul = document.querySelector('body > app-root > app-game > div > div.main-container > div.w-100.h-100 > div > div.game-play > div.bet-controls > app-bet-controls > div > app-bet-control:nth-child(1) > div > div.second-row > div.cashout-block > div.cashout-spinner-wrapper > div > app-spinner > div > div.input.full-width > input');
+      inputElementMul.value = ''; // Cambia el valor del input a "100"
+      inputElementMul.dispatchEvent(new Event('input')); // Dispara un evento 'input' para que se refleje el cambio
+     });
+   
+     //await page.screenshot({ path: 'Verificar_5.png' });
+    const inputElement = await frameInterno.$('body > app-root > app-game > div > div.main-container > div.w-100.h-100 > div > div.game-play > div.bet-controls > app-bet-controls > div > app-bet-control:nth-child(1) > div > div.second-row > div.cashout-block > div.cashout-spinner-wrapper > div > app-spinner > div > div.input.full-width > input');
+    await inputElement.type('1.05');
+    await new Promise(resolve => setTimeout(resolve, 1000)); // Espera 5000 milisegundos (5 segundos)
+   // await page.screenshot({ path: 'Verificar_5.png' });
+    //await inputElement.click({ button: 'left' });
+    /*await inputElement.click({ clickCount: 3 }); // Seleccionar todo el texto y borrar
+    await inputElement.press('Backspace'); // Oprimir la tecla "Backspace" para borrar
+    await new Promise(resolve => setTimeout(resolve, 1000)); // Espera 5000 milisegundos (5 segundos)
+    await inputElement.type('1.05');
+    await new Promise(resolve => setTimeout(resolve, 1000)); // Espera 5000 milisegundos (5 segundos)
+    await page.screenshot({ path: 'Verificar_5.png' });
+    */
+    // Verifica que el elemento haya sido encontrado
+
+
+
+
+if (inputElement) {
+  // Obtiene el valor del elemento
+  const valorElemento = await inputElement.getProperty('value');
+  const valorIngresado = await valorElemento.jsonValue();
+  // Verifica que el valor sea "1.05"
+  if (valorIngresado.trim() === '1.05') {
+      console.log('El valor ingresado es 1.05.');  
+      await  frameInterno.waitForSelector('body > app-root > app-game > div > div.main-container > div.w-100.h-100 > div > div.game-play > div.bet-controls > app-bet-controls > div > app-bet-control:nth-child(1) > div > div.first-row.auto-game-feature > div.buttons-block > button');
+      await  frameInterno.click('body > app-root > app-game > div > div.main-container > div.w-100.h-100 > div > div.game-play > div.bet-controls > app-bet-controls > div > app-bet-control:nth-child(1) > div > div.first-row.auto-game-feature > div.buttons-block > button');
+     // await page.screenshot({ path: 'Verificar_6.png' });
+      await new Promise(resolve => setTimeout(resolve, 60000)); // Espera 1000 milisegundos (5 segundos) 
+    } else {
+      console.log('El valor ingresado no es 1.05.');
+      await new Promise(resolve => setTimeout(resolve, 5000)); // Espera 1000 milisegundos (5 segundos) 
+      //agregamos el valor 1.05 de retiro automatico
+      // await frameInterno.waitForSelector('body > app-root > app-game > div > div.main-container > div.w-100.h-100 > div > div.game-play > div.bet-controls > app-bet-controls > div > app-bet-control:nth-child(1) > div > div.second-row > div.cashout-block > div.cashout-spinner-wrapper > div > app-spinner > div > div.input.full-width > input');
+       //const inputElement = await frameInterno.$('body > app-root > app-game > div > div.main-container > div.w-100.h-100 > div > div.game-play > div.bet-controls > app-bet-controls > div > app-bet-control:nth-child(1) > div > div.second-row > div.cashout-block > div.cashout-spinner-wrapper > div > app-spinner > div > div.input.full-width > input');
+       await inputElement.click({ clickCount: 3 }); // Seleccionar todo el texto y borrar
+       await inputElement.press('Backspace'); // Oprimir la tecla "Backspace" para borrar
+       await new Promise(resolve => setTimeout(resolve, 1000)); // Espera 5000 milisegundos (5 segundos)
+       await inputElement.type('1.05');
+       await new Promise(resolve => setTimeout(resolve, 1000)); // Espera 5000 milisegundos (5 se os)
+
+       const valorElemento = await inputElement.getProperty('value');
+        const valorIngresado = await valorElemento.jsonValue();
+       if (valorIngresado.trim() === '1.05') {
+        // Obtiene el valor del elemento
+          console.log('El valor 2 ingresado es 1.05.');  
+          await  frameInterno.waitForSelector('body > app-root > app-game > div > div.main-container > div.w-100.h-100 > div > div.game-play > div.bet-controls > app-bet-controls > div > app-bet-control:nth-child(1) > div > div.first-row.auto-game-feature > div.buttons-block > button');
+          await  frameInterno.click('body > app-root > app-game > div > div.main-container > div.w-100.h-100 > div > div.game-play > div.bet-controls > app-bet-controls > div > app-bet-control:nth-child(1) > div > div.first-row.auto-game-feature > div.buttons-block > button');
+          await page.screenshot({ path: 'Verificar_6.png' });
+          await new Promise(resolve => setTimeout(resolve, 60000)); // Espera 1000 milisegundos (5 segundos) 
+        
+        
+      
+      }
+      else {console.log('El valor ingresado 2 no es 1.05.');}
+      }
+} else {
+  console.log('No se encontró el elemento.');
+}  
+    
+   
+    success = true;
+  //  await page.screenshot({ path: 'Verificar_7.png' });
+
+  } catch (error) {
+    console.error('Ocurrió un error durante el proceso:', error);
+  } finally {
+    // Cerrar el navegador al finalizar
+    await browser.close();
+  }
+})();
---
+++ youtube.js
@@ -0,0 +1,133 @@
+const puppeteer = require('puppeteer');
+const fs = require("fs").promises;
+const sleep = (milliseconds) => {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+};
+
+(async () => {
+
+  
+
+
+  let success = false; // Variable para indicar si la ejecución fue exitosa
+  const minTiempo = 60000; // 6 segundos
+  const maxTiempo = 180000; // 18 segundos
+
+
+  const browser = await puppeteer.launch({ headless:true, args: ['--no-sandbox'] });
+  //const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  try {
+   // await page.setViewport({ width: 1280, height: 800 });
+   // await page.setViewport({ width: 1280, height: 800 });
+    //load cookies
+    await page.goto("https://accounts.google.com/signin/v2/identifier", {
+      waitUntil: "networkidle2",
+    });
+    await page.screenshot({ path: 'Verificar_11.png' });
+    await page.type("#identifierId", "egfcinverisiones@gmail.com");
+    await page.click("#identifierNext");
+    
+    await page.waitForSelector("#password", {
+      visible: true,
+      hidden: false,
+    });
+    
+    await page.type(
+      "#password",
+      "Qwert1619"
+    );
+   
+    await sleep(1000);
+    await page.click("#passwordNext");
+  
+    await sleep(10000);
+    // Navegar a la página de YouTube y esperar a que la carga sea completa
+    await page.setViewport({ width: 1280, height: 800 });
+  const navigationPromise = page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+  await page.setDefaultNavigationTimeout(0); 
+  await page.goto('https://www.youtube.com/');
+  await navigationPromise;
+
+
+ // await page.screenshot({ path: 'Verificar_1.png' });
+    //await page.goto('https://www.youtube.com/');
+    await page.waitForSelector('input#search', { visible: true });
+
+
+  // Hacer clic en el botón "Accept all"
+  
+    await page.screenshot({ path: 'Verificar_3.png' });
+    await page.type('input#search', 'Análisis Corto en Aviator  Eduardo Fuentes'); // Reemplaza 'Tu búsqueda aquí' con el video que deseas buscar
+    await page.click('button#search-icon-legacy');
+    //await page.screenshot({ path: 'Verificar_4.png' });
+    await new Promise(resolve => setTimeout(resolve, 6000)); // Espera 5000 milisegundos (5 segundos)
+     // Esperar a que aparezcan los resultados y hacer clic en el primer video
+// Utilizar page.evaluate para encontrar y hacer clic en el enlace con el texto específico
+await page.evaluate(() => {
+  const link = Array.from(document.querySelectorAll('a.yt-simple-endpoint')).find(a => a.textContent.includes('Análisis Corto en Aviator'));
+  if (link) {
+    link.click();
+  } else {
+    console.error('Enlace no encontrado');
+  }
+});
+
+ // await page.waitForSelector('ytd-video-renderer');
+  //const firstVideo = await page.$('ytd-video-renderer');
+  //await firstVideo.click();
+  // Generar un número aleatorio entre minTiempo y maxTiempo
+  const tiempoAleatorio = Math.floor(Math.random() * (maxTiempo - minTiempo + 1)) + minTiempo;
+  await new Promise(resolve => setTimeout(resolve, tiempoAleatorio)); // Espera 5000 milisegundos (5 segundos)
+  //await page.screenshot({ path: 'Verificar_5.png' });
+  //await new Promise(resolve => setTimeout(resolve, 6000)); // Espera 5000 milisegundos (5 segundos)
+  await page.screenshot({ path: 'Verificar_6.png' });
+
+  await page.$eval('input#search', input => input.value = '');
+  //await page.type('input#search', 'Análisis Corto en Aviator');
+  await page.type('input#search', 'Análisis Corto en Aviator  Eduardo Fuentes'); // Reemplaza 'Tu búsqueda aquí' con el video que deseas buscar
+  await page.evaluate(() => {
+    const link = Array.from(document.querySelectorAll('a.yt-simple-endpoint')).find(a => a.textContent.includes('Programa para analizar soportes y Resistencias en Aviator.'));
+    if (link) {
+      link.click();
+    } else {
+      console.error('Enlace no encontrado');
+    }
+  });
+  
+  //await page.click('button#search-icon-legacy');
+ // await page.screenshot({ path: 'Verificar_4.png' });
+ // await new Promise(resolve => setTimeout(resolve, 6000)); // Espera 5000 milisegundos (5 segundos)
+ const tiempoAleatorio2 = Math.floor(Math.random() * (maxTiempo - minTiempo + 1)) + minTiempo;
+  await new Promise(resolve => setTimeout(resolve, tiempoAleatorio2)); // Espera 5000 milisegundos (5 segundos)
+  await page.screenshot({ path: 'Verificar_7.png' });
+
+  ////otro video 
+
+
+  await page.$eval('input#search', input => input.value = '');
+  //await page.type('input#search', 'Análisis Corto en Aviator');
+  await page.type('input#search', 'Análisis Corto en Aviator  Eduardo Fuentes'); // Reemplaza 'Tu búsqueda aquí' con el video que deseas buscar
+  await page.evaluate(() => {
+    const link = Array.from(document.querySelectorAll('a.yt-simple-endpoint')).find(a => a.textContent.includes('Aviator Soportes y Resistencias con python'));
+    if (link) {
+      link.click();
+    } else {
+      console.error('Enlace no encontrado');
+    }
+  });
+  
+  //await page.click('button#search-icon-legacy');
+ // await page.screenshot({ path: 'Verificar_4.png' });
+ // await new Promise(resolve => setTimeout(resolve, 6000)); // Espera 5000 milisegundos (5 segundos)
+ const tiempoAleatorio3 = Math.floor(Math.random() * (maxTiempo - minTiempo + 1)) + minTiempo;
+  await new Promise(resolve => setTimeout(resolve, tiempoAleatorio3)); // Espera 5000 milisegundos (5 segundos)
+  await page.screenshot({ path: 'Verificar_8.png' });
+    
+  } catch (error) {
+    console.error('Ocurrió un error durante el proceso:', error);
+  } finally {
+    // Cerrar el navegador al finalizar
+    await browser.close();
+  }
+})();
